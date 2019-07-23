f8cd8edd2f0fa864295dc7014af8514e0f57f8ef
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-23 22:03:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @num_1 = params[:num1]
-    @num_2 = params[:num2]
-    "Goodbye, #{@person_name}."
+    @product = params[:num1] * params[:num2]
+    "#{@product}"
   end
 end
